ebbb28048adff4f160e31fc23d4b8a1609e53e0d
Mike <mrpkeleven+github@gmail.com>
2025-07-06 17:07:24 -0400

    Add Corroded Potion
    Introduces the Corroded Potion to the mod, registering it with a 6-minute effect duration. Updates the English localization file to include display names for the Corroded Potion and its variants.

--- src/main/java/com/github/sculkhorde/core/ModPotions.java
+++ src/main/java/com/github/sculkhorde/core/ModPotions.java
@@ -1,6 +1,6 @@
 package com.github.sculkhorde.core;
 
-import net.minecraft.client.renderer.EffectInstance;
+import com.github.sculkhorde.util.TickUnits;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.item.alchemy.Potion;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -11,7 +11,7 @@ import net.minecraftforge.registries.RegistryObject;
 public class ModPotions {
     public static final DeferredRegister<Potion> POTIONS = DeferredRegister.create(ForgeRegistries.POTIONS, SculkHorde.MOD_ID);
 
-    //public static final RegistryObject<Potion> PURITY_POTION = POTIONS.register("purity_potion", () -> new Potion(new MobEffectInstance(ModMobEffects.SCULK_INFECTION.get(), 200, 0)));
+    public static final RegistryObject<Potion> CORRODED_POTION = POTIONS.register("corroded_potion", () -> new Potion(new MobEffectInstance(ModMobEffects.CORRODED.get(), TickUnits.convertMinutesToTicks(6), 0)));
 
     public static void register(IEventBus eventBus) {
         POTIONS.register(eventBus);
